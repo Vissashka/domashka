f1487dca676b39fe1965bc4cc6593a20d3e42792
Viktoriya Vasenina <viktoriya-vasenina@yandex.ru>
2025-12-09 07:53:53 +0200

    Bump minimum Python version to 3.9

--- src/generators.py
+++ src/generators.py
@@ -1,33 +1,38 @@
 from typing import Generator, Optional, Union
 from itertools import count
 
+
 def filter_by_currency(transactions: list, currency_code: str) -> Generator:
     """
-    Генерирует транзакции заданной валюты.
+    Фильтрация транзакций по заданной валюте.
 
-    Parameters:
-        transactions (list): Список транзакций, каждая из которых представлена словарем.
-                            Каждая транзакция включает вложенное поле operationAmount.currency.code,
-                            которое хранит валюту операции.
-        currency_code (str): Код валюты, по которой фильтруются транзакции.
-
-    Yields:
-        dict: Транзакции, соответствующие указанной валюте.
+    :param transactions: Список транзакций, представленных словарями.
+    :param currency_code: Валюта для фильтрации.
+    :return: Итератор, содержащий подходящие транзакции.
     """
-    yield from (tx for tx in transactions if tx['operationAmount']['currency']['code'] == currency_code)
+    for tx in transactions:
+        try:
+            amount_data = tx.get("operationAmount") or {}
+            currency_data = amount_data.get("currency") or {}
+
+            if currency_data.get("code") == currency_code:
+                yield tx
+        except Exception as e:
+            pass  # Или можно записать ошибку куда-то дополнительно
+
 
 def transaction_descriptions(transactions: list) -> Generator[str, None, None]:
     """
-    Генерирует описания транзакций.
+    Получение описаний транзакций.
 
-    Parameters:
-        transactions (list): Список транзакций, каждая из которых представлена словарем.
-                            Поле description содержит описание операции.
-
-    Yields:
-        str: Описание очередной транзакции.
+    :param transactions: Список транзакций.
+    :return: Итератор строковых описаний транзакций.
     """
-    yield from (tx['description'] for tx in transactions)
+    for tx in transactions:
+        try:
+            yield tx["description"]
+        except KeyError:
+            continue  # Пропускаем транзакции без описания
 
 def card_number_generator(start: int = 1, stop: Optional[int] = None) -> Generator[str, None, None]:
     """
